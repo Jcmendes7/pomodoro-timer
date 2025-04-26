f34364f5d757340d4df52e69b9450258e6769cf9
Jose Carlos <josecarlosms7f@gmail.com>
2025-04-26 15:18:47 -0300

    form add

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import "./styles/global.css";
 import { Container } from "./components/Container";
 import { Logo } from "./components/Logo";
 import { Menu } from "./components/Menu";
+import { Countdown } from "./components/Countdown";
 
 export function App() {
   return (
@@ -12,6 +13,25 @@ export function App() {
       </Container>
       <Container>
         <Menu />
+      </Container>
+      <Container>
+        <Countdown />
+      </Container>
+      <Container>
+        <form className="form" action="#">
+          <div className="formRow">
+            <label htmlFor="task">Task</label>
+            <input type="text" name="task" id="task" />
+          </div>
+          <div className="formRow">
+            <p>Lorem ipsum dolor sit amet.</p>
+          </div>
+          <div className="formRow">
+            <p>Ciclos</p>
+            <p>0000000000</p>
+          </div>
+          <button>Enviar</button>
+        </form>
       </Container>
     </>
   );
---
+++ src/components/Countdown/index.tsx
@@ -0,0 +1,8 @@
+import styles from "./styles.module.css";
+export function Countdown() {
+  return (
+    <>
+      <span className={styles.countdown}>00:00</span>
+    </>
+  );
+}
